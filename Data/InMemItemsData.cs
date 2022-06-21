aed609b728a5cdce45c5572a32781b6e9746de32
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-06-21 12:05:08 +0200

    Add the InMemItemsData

---
+++ Data/InMemItemsData.cs
@@ -0,0 +1,26 @@
+﻿using CatalogAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Data
+{
+    public class InMemItemsData
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+
+        };
+
+        public IEnumerable<Item> GetItems() => items;
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
