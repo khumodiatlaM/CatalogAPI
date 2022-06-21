6e527b09b68311feda260cfceda86f3688608d2c
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-06-21 12:40:04 +0200

    Add the IItemsData Interface

---
+++ Data/IItemsData.cs
@@ -0,0 +1,12 @@
+﻿using CatalogAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CatalogAPI.Data
+{
+    public interface IItemsData
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+    }
+}
