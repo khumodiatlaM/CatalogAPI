da3966924569364116913ad9e22e1140affd13f7
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-06-21 12:40:32 +0200

    Add the ItemsController

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+﻿using CatalogAPI.Data;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsData data;
+
+        public ItemsController(IItemsData data)
+        {
+            this.data =  data;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = data.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = data.GetItem(id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+    }
+}
